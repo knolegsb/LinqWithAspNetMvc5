0174567e97941f54f8cdb1f9f1b23f0f28f2e2cb
knolegsb <seon2you@hotmail.com>
2015-02-17 23:03:55 -0800

    linq

---
+++ LinqWithAsp.NetMvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LinqWithAsp.NetMvc5.Startup))]
+namespace LinqWithAsp.NetMvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
